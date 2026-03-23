c769a5006c618cf6e4d088e2c698d584a072b45f
Olaf Leidinger <oleid@mescharet.de>
2026-03-23 15:34:51 +0100

    FEATURE: Support builds without exceptions

--- include/zenoh/api/base.hxx
+++ include/zenoh/api/base.hxx
@@ -26,6 +26,7 @@ namespace zenoh {
 /// @brief Error code returned by Zenoh API
 typedef ::z_result_t ZResult;
 
+#ifdef __cpp_exceptions
 /// @brief Zenoh-specific Exception
 class ZException : public std::runtime_error {
    public:
@@ -33,11 +34,16 @@ class ZException : public std::runtime_error {
     ZException(const std::string& message, ZResult err)
         : std::runtime_error(message + "(Error code: " + std::to_string(err) + " )"), e(err) {}
 };
+#define THROW_ZEXCEPTION(msg, err) throw ZException(msg, err)
+
+#else
+#define THROW_ZEXCEPTION(msg, err) exit(err)
+#endif
 
 #define __ZENOH_RESULT_CHECK(err, err_ptr, message)        \
     if (err_ptr == nullptr) {                              \
         ZResult __ze = static_cast<ZResult>(err);          \
-        if (__ze != Z_OK) throw ZException(message, __ze); \
+        if (__ze != Z_OK) THROW_ZEXCEPTION(message, __ze); \
     } else {                                               \
         *err_ptr = static_cast<ZResult>(err);              \
     }
